fcccd72585d90dcb053b71a568e685f353d66731
thullyDev <thully.developer@gmail.com>
2024-08-08 19:45:40 +0200

    [update] did the footer, (no testing)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,30 @@
-export function Footer() {
+import type { FooterProps } from "./types";
+
+export function Footer({ sideFooterImg, socialLinks }: FooterProps) {
 	return (
-		<footer></footer>
+		<footer>
+			<div className="socials-con">
+				<div className="inner-con">
+					<span className="footer-side-img-con">
+						<img src={sideFooterImg} alt="side image" className="side-img" />
+					</span>
+					<div className="social-links-con">
+						<ul className="socials-list">
+						{
+							socialLinks.map(({name, link, icon}, index) => {
+								return (
+									<li key={index} className="social-item">
+										{ /* <Icon icon={icon} /> */ }
+										<a href={link} className="social-link">{name}</a>
+									</li>
+								)
+							})
+						}
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
 	)
 }
 
---
+++ src/components/Footer/types.ts
@@ -0,0 +1,10 @@
+export type SocialLink = {
+	icon: string;
+	name: string;
+	link: string;
+}
+
+export type FooterProps = {
+	sideFooterImg: string;
+	socialLinks: SocialLink[]
+}
