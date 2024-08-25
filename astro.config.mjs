e41644061159f569f6893586058e8834a8dcb2e9
thullyDev <thully.developer@gmail.com>
2024-08-25 11:12:30 +0200

    [update] doing shit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+// import node from "@astrojs/node";
+// import netlify from "@astrojs/netlify";
 
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   devToolbar: {
-    enabled: false,
+    enabled: false
   },
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel()
 });
